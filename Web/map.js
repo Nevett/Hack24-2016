81e5b3034a213e23971ad26a41c98ece780312c8
Nevett <nevett@gmail.com>
2016-03-19 16:50:33 +0000

    icons, whitespace

--- Web/map.js
+++ Web/map.js
@@ -1,45 +1,51 @@
 function initMap() {
-  window.map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 52.953350, lng: -1.150350},
-    zoom: 19,
-    styles: [{
-      featureType: 'poi',
-      stylers: [{ visibility: 'off' }]  // Turn off points of interest.
-    }, {
-      featureType: 'transit.station',
-      stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
-    }],
-    disableDoubleClickZoom: true
-  });
-}
+	window.map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 52.953350, lng: -1.150350},
+		zoom: 19,
+		styles: [{
+			featureType: 'poi',
+			stylers: [{ visibility: 'off' }]  // Turn off points of interest.
+		}, {
+			featureType: 'transit.station',
+			stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
+		}],
+		disableDoubleClickZoom: true
+	});
 
-var pusher = new Pusher('90328f0186c7537be223', {
-      cluster: 'eu',
-      encrypted: true,
-	  authTransport: 'jsonp',
-	  authEndpoint: 'http://dump.nevett.me/hack24/auth.php'
-    });
-
-var channel = pusher.subscribe('private-bite');
-
-var markers = {};
-
-var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-
-channel.bind('player-moved', function(data) {
-	var lat = data.lat;
-	var lng  = data.lng;
-	var marker = markers[data.playerId];
-	console.log(data);
-	if (!marker) {
-		marker = markers[data.playerId] = new google.maps.Marker({
-			map: map,
-			icon: image
+	var pusher = new Pusher('90328f0186c7537be223', {
+		  cluster: 'eu',
+		  encrypted: true,
+		  authTransport: 'jsonp',
+		  authEndpoint: 'http://dump.nevett.me/hack24/auth.php'
 		});
-	}
-	
-	marker.setPosition( new google.maps.LatLng( lat, lng ) );
-});
 
+	var channel = pusher.subscribe('private-bite');
 
-  
+	var markers = {};
+
+	var human = { 
+		url: 'human.png', 
+		size: new google.maps.Size(115, 115),
+		scaledSize: new google.maps.Size(25, 25)
+	};
+	var zombie = { 
+		url: 'zombie.png', 
+		size: new google.maps.Size(101, 101),
+		scaledSize: new google.maps.Size(25, 25)
+	};
+
+	channel.bind('player-moved', function(data) {
+		var lat = data.lat;
+		var lng  = data.lng;
+		var marker = markers[data.playerId];
+		console.log(data);
+		if (!marker) {
+			marker = markers[data.playerId] = new google.maps.Marker({
+				map: map,
+				icon: human
+			});
+		}
+		
+		marker.setPosition( new google.maps.LatLng( lat, lng ) );
+	});
+}
