380841b1ea7448c2bcba9f3e5bb07c59aef9f502
Danny Parker <danny.ukdm@gmail.com>
2016-03-19 18:09:20 +0000

    Added safezones and dangerzones

--- Web/map.js
+++ Web/map.js
@@ -48,4 +48,63 @@ function initMap() {
 		
 		marker.setPosition( new google.maps.LatLng( lat, lng ) );
 	});
+
+	var safezones = {
+  sZone1: {
+    center: {lat: 52.954187, lng: -1.149632},
+    zoneSize: 50
+  },
+  sZone2: {
+    center: {lat: 52.953449, lng: -1.148950},
+    zoneSize: 100
+  },
+  sZone3: {
+    center: {lat: 52.953118, lng: -1.150597},
+    zoneSize: 30
+  }
+};
+
+	var dangerzones = {
+  dZone1: {
+    center: {lat: 52.953412, lng: -1.149951},
+    zoneSize: 500
+  },
+  dZone2: {
+    center: {lat: 52.953734, lng: -1.151644},
+    zoneSize: 280
+  },
+  dZone3: {
+    center: {lat: 52.953792, lng: -1.148353},
+    zoneSize: 150
+  }
+};
+
+for (var zone in safezones) {
+    // Add the circle for this city to the map.
+    var cityCircle = new google.maps.Circle({
+      strokeColor: '#00ff00',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#00ff00',
+      fillOpacity: 0.35,
+      map: map,
+      center: safezones[zone].center,
+      radius: Math.sqrt(safezones[zone].zoneSize) 
+    });
+  }
+
+for (var zone in dangerzones) {
+    // Add the circle for this city to the map.
+    var cityCircle = new google.maps.Circle({
+      strokeColor: '#ff0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#ff0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: dangerzones[zone].center,
+      radius: Math.sqrt(dangerzones[zone].zoneSize) 
+    });
+  }  
+
 }
