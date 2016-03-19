b3a8e44a36b70c55099a855133e714f73c3f8b5e
Danny Parker <danny.ukdm@gmail.com>
2016-03-19 20:45:44 +0000

    ZONES!

--- Web/map.js
+++ Web/map.js
@@ -22,6 +22,7 @@ function initMap() {
 	var channel = pusher.subscribe('private-bite');
 
 	var markers = {};
+	var zones = {};
 
 	var human = { 
 		url: 'human.png', 
@@ -36,77 +37,42 @@ function initMap() {
 		anchor: new google.maps.Point(32, 32)
 	};
 
-	channel.bind('player-moved', function(data) {
-		var lat = data.lat;
-		var lng  = data.lng;
-		var marker = markers[data.playerId];
-		console.log(data);
-		if (!marker) {
-			marker = markers[data.playerId] = new google.maps.Marker({
-				map: map,
-				icon: human
-			});
-		}
-		
-		marker.setPosition( new google.maps.LatLng( lat, lng ) );
+	channel.bind('game-update', function(data) {	
+		data.players.forEach(function (player){
+			var lat = player.location.lat;
+			var lng  = player.location.lng;
+			var marker = markers[player.id];
+			console.log(player);
+			if (!marker) {
+				marker = markers[player.id] = new google.maps.Marker({
+					map: map,
+					icon: human
+				});
+			}
+			marker.setPosition( new google.maps.LatLng( lat, lng ) );
+		});
+
+		data.zones.forEach(function (zoneData){
+
+			var zone = zones[zoneData.id]
+
+			if (!zone){	
+
+				var colour = zoneData.type === "safe" ? "#00ff00" : "#daa520";
+
+			 	zones[zoneData.id] = new google.maps.Circle({
+			      strokeColor: colour,
+			      strokeOpacity: 0.8,
+			      strokeWeight: 2,
+			      fillColor: colour,
+			      fillOpacity: 0.15,
+			      map: map,
+			      center: zoneData.center,
+			      radius: Math.sqrt(zoneData.zoneSize) 
+    			});
+
+			}
+
+		});
 	});
-
-	var safeZones = [
-  		{
-    		center: {lat: 52.954187, lng: -1.149632},
-    		zoneSize: 50
-  		},
-  		{
-    		center: {lat: 52.953449, lng: -1.148950},
-    		zoneSize: 100
-  		},
-  		{
-   			center: {lat: 52.953118, lng: -1.150597},
-   			zoneSize: 50
-  		}
-	];
-
-	var dangerZones = [
-  		{
-    		center: {lat: 52.953412, lng: -1.149951},
-    		zoneSize: 500
-  		},
-  		{
-    		center: {lat: 52.953600, lng: -1.151644},
-    		zoneSize: 280
-  		},
-  		{
-    		center: {lat: 52.953792, lng: -1.148353},
-    		zoneSize: 150
-  		}
-	];
-
-safeZones.forEach(function(zone){
-    // Add the circle for this city to the map.
-    var cityCircle = new google.maps.Circle({
-      strokeColor: '#00ff00',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#00ff00',
-      fillOpacity: 0.15,
-      map: map,
-      center: zone.center,
-      radius: Math.sqrt(zone.zoneSize) 
-    });
-  });
-
-dangerZones.forEach(function(zone){
-    // Add the circle for this city to the map.
-    var cityCircle = new google.maps.Circle({
-      strokeColor: '#ff0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#ff0000',
-      fillOpacity: 0.15,
-      map: map,
-      center: zone.center,
-      radius: Math.sqrt(zone.zoneSize) 
-    });
-  }); 
-
 }
